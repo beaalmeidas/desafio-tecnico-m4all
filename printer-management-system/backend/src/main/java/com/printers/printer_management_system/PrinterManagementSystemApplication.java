4e9966af84490e4457df2659b66574ff9ef92afa
beaalmeidas <beatrizalmeidabss@gmail.com>
2025-07-05 09:39:05 -0300

    chore: iniciando banckend + conectando o BD

---
+++ printer-management-system/backend/src/main/java/com/printers/printer_management_system/PrinterManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.printers.printer_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinterManagementSystemApplication.class, args);
+	}
+
+}
---
+++ printer-management-system/backend/src/test/java/com/printers/printer_management_system/PrinterManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printers.printer_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinterManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
